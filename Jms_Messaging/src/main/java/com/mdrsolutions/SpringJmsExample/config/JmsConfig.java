8edcb5ba897e042cc7325c713d8d0d6d78bc4318
Steven Eck <steeck1@yahoo.com>
2021-01-26 18:24:44 -0500

    first commit

---
+++ Jms_Messaging/src/main/java/com/mdrsolutions/SpringJmsExample/config/JmsConfig.java
@@ -0,0 +1,87 @@
+package com.mdrsolutions.SpringJmsExample.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.connection.JmsTransactionManager;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@EnableJms
+@EnableTransactionManagement
+@Configuration
+public class JmsConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JmsConfig.class);
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.user}")
+    private String user;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+
+        CachingConnectionFactory factory = new CachingConnectionFactory(
+                new ActiveMQConnectionFactory(user, password, brokerUrl)
+        );
+        factory.setClientId("StoreFront");
+        factory.setSessionCacheSize(100);
+        return factory;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+        factory.setTransactionManager(jmsTransactionManager());
+        factory.setErrorHandler(t -> {
+            LOGGER.info("Handling error in listening for messages, error: " + t.getMessage());
+        });
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager jmsTransactionManager(){
+        return new JmsTransactionManager(connectionFactory());
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
+        jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+        jmsTemplate.setDeliveryPersistent(true);
+        jmsTemplate.setSessionTransacted(true);
+        return jmsTemplate;
+
+    }
+
+
+}
